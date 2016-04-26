c9483662166be56f3726a52db388f601e81a7ef4
cmtzco <m9157317614@gmail.com>
2016-04-26 13:13:00 -0500

    Fixed the download function and just moved it into the main lr function since it would not be repeated.

--- lr.py
+++ lr.py
@@ -7,17 +7,18 @@ import pysftp
 import getpass
 
 @click.command()
-@click.option('--path', default='/var/log/', help='Path to search remote host for logs')
-@click.option('--local', default='/home/user', help='Path to store the logs locally')
+@click.option('--path', default='/var/log/', help='Path to search remote host for logs. Make sure to end with a /')
+@click.option('--local', default='/home/user/', help='Path to store the logs locally.  Make sure to end with a /')
 @click.option('--ext', default='log', help='Extension of files to pull down from the remote server')
 @click.argument('user')
 @click.argument('host')
 @click.argument('foldername')
 
-def download(user, hostname):
-  password = getpass.getpass()
-  with pysftp.Connection(hostname, username=user, password=password) as sftp:
-    with sftp.cd  ##left off building sftp function
+#def download(user, hostname, pathtoremote, pathtolocal, foldername):
+#  password = getpass.getpass()
+#  with pysftp.Connection(hostname, username=user, password=password) as sftp:
+#    sftp.get_d('{}{}.tar.gz'.format(pathtoremote, foldername), pathtolocal, preserve_mtime=True)
+
 def lr(path, local, ext, user, host, foldername):
   """LogRoller allows for organized log downloads"""
   click.echo('\tPath: {}'.format(path))
@@ -43,7 +44,9 @@ def lr(path, local, ext, user, host, foldername):
     print >>sys.stderr, "ERROR: %s" % error
   else:
     print result
-  
+    pw  = getpass.getpass()
+    sftp = pysftp.Connection(host, username=user, password=pw)
+    sftp.get('{}{}.tar.gz'.format(path, foldername), localpath='{}{}'.format(local, foldername),  preserve_mtime=True)
 
 if __name__ == '__main__':
   lr()
