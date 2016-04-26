8f1fbed88bef8aa0573c86196c86eab322c6c958
cmtzco <m9157317614@gmail.com>
2016-04-26 13:27:20 -0500

    Implemented options and a cleaner approach to the way it was written. Python<3

--- lr.py
+++ lr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+!/usr/bin/python
 ###!/usr/local/bin//python  ##USED FOR MAC ONLY
 import click
 import subprocess
@@ -14,39 +14,32 @@ import getpass
 @click.argument('host')
 @click.argument('foldername')
 
-#def download(user, hostname, pathtoremote, pathtolocal, foldername):
-#  password = getpass.getpass()
-#  with pysftp.Connection(hostname, username=user, password=password) as sftp:
-#    sftp.get_d('{}{}.tar.gz'.format(pathtoremote, foldername), pathtolocal, preserve_mtime=True)
-
 def lr(path, local, ext, user, host, foldername):
-  """LogRoller allows for organized log downloads"""
-  click.echo('\tPath: {}'.format(path))
-  click.echo('\tLocal Path: {}'.format(local))
-  click.echo('\tExtension: {}'.format(ext))
-  click.echo('\tUser: {}'.format(user))
-  click.echo('\tHost: {}'.format(host))
-  click.echo('\tFolder Name: {}'.format(foldername))
-  command = 'cd {} && mkdir {} && cp *.{} {}/ && tar -cvzf {}.tar.gz {}'.format(path, 
-                                                                                foldername, 
-                                                                                ext, 
-                                                                                foldername, 
-                                                                                foldername, 
-                                                                                foldername)
-  click.echo(command)
-  ssh = subprocess.Popen(["ssh", "{}@{}".format(user, host), command],
-                         shell=False,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-  result = ssh.stdout.readlines()
-  if result == []:
-    error = ssh.stderr.readlines()
-    print >>sys.stderr, "ERROR: %s" % error
-  else:
-    print result
-    pw  = getpass.getpass()
-    sftp = pysftp.Connection(host, username=user, password=pw)
-    sftp.get('{}{}.tar.gz'.format(path, foldername), localpath='{}{}'.format(local, foldername),  preserve_mtime=True)
+    """LogRoller allows for organized log downloads"""
+    click.echo('\tPath: {}'.format(path))
+    click.echo('\tLocal Path: {}'.format(local))
+    click.echo('\tExtension: {}'.format(ext))
+    click.echo('\tUser: {}'.format(user))
+    click.echo('\tHost: {}'.format(host))
+    click.echo('\tFolder Name: {}'.format(foldername))
+    command = 'cd {0} && mkdir {1} && cp *.{2} {1}/ && tar -cvzf {1}.tar.gz {1}'.format(path,
+                                                                                        foldername,
+                                                                                        ext)
+    click.echo(command)
+    ssh = subprocess.Popen(["ssh", "{}@{}".format(user, host), command],
+                           shell=False,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+    result = ssh.stdout.readlines()
+    if result == []:
+        error = ssh.stderr.readlines()
+        print >>sys.stderr, "ERROR: {}".format(error)
+    else:
+        print result
+        pw  = getpass.getpass()
+        sftp = pysftp.Connection(host, username=user, password=pw)
+        sftp.get('{}{}.tar.gz'.format(path, foldername), localpath='{}{}'.format(local, foldername),  preserve_mtime=True)
 
 if __name__ == '__main__':
-  lr()
+    lr()
+
